2b9c891b72229003696e8e2c73afb65a30283cf9
carldesu <skakeru0920@gmail.com>
2019-09-28 12:41:01 +0000

    [AC] abc142 abc142_a

---
+++ atcoder.jp/abc142/abc142_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int N, odd;
+  cin >> N;
+  odd = (N+1)/2;
+  double A = odd,B = N;
+  
+  cout << A/N << endl;
+}
