e6fe97180b3f1db6ce8ec0ecaf70b0692ee1d244
carldesu <skakeru0920@gmail.com>
2020-04-14 13:02:46 +0000

    [AC] abc148 abc148_a

---
+++ atcoder.jp/abc148/abc148_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(x,to) for(int (x)=0;(x)<(to);(x)++)
+#define rep2(x,fr,to) for(int (x)=(fr);(x)<(to);(x)++)
+#define sz(v) (int)(v).size()
+using namespace std;
+typedef long long ll;
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	int A, B;
+	cin >> A >> B;
+	cout << 6 - A - B << '\n';
+
+	return 0;
+}
