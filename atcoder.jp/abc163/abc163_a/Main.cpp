a054078518d0e6ba339e63cb0daa94a1fbe9b91a
carldesu <skakeru0920@gmail.com>
2020-04-19 12:11:49 +0000

    [AC] abc163 abc163_a

---
+++ atcoder.jp/abc163/abc163_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(x,to) for(int (x)=0;(x)<(to);(x)++)
+#define rep2(x,fr,to) for(int (x)=(fr);(x)<(to);(x)++)
+#define sz(v) (int)(v).size()
+using namespace std;
+typedef long long ll;
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	double PI=3.14159265358979323846;
+	double R;
+	cin >> R;
+
+	cout << R * 2 * PI << '\n';
+	return 0;
+}
